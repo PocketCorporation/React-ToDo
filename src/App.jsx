4b290fe9410bf6bb57f0fc19744aafa9f30c5a28
pocketcorp <pocketcorp@protonmail.com>
2023-09-02 04:16:19 -0700

    Compmonents completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./styles.css"
 import { useState } from 'react'
 import { NewTodoForm } from './NewTodoForm'
+import { TodoList } from './TodoList'
 
 export default function App() {
-  const [newItem, setNewItem] = useState('')
-  
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title){
+    setTodos(currentTodos=>{
+      return[
+      ...currentTodos,
+      { id:crypto.randomUUID(), title, completed: false}
+      ]
+    })
+  }  
   
   function toggleTodo(id, completed) {
     setTodos(currentTodos=>{
@@ -26,23 +35,9 @@ export default function App() {
 
   return ( 
   <>
-    <NewTodoForm />
+    <NewTodoForm onSubmit={addTodo}/>
     <h1 className="header"> Todo List </h1>
-    <ul className="list">
-      {todos.length === 0 && 'No Todos'}
-      {todos.map(todo=>{
-        return (
-          <li key={todo.id}>
-            <label htmlFor="">
-              <input type="checkbox" checked={todo.completed}
-              onChange={e=>toggleTodo(todo.id, e.target.checked)} />
-              {todo.title}
-            </label>
-            <button onClick={()=>deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-          </li>
-        )
-      })}
-    </ul>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </>
   )
 }
--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
 
-export function NewTodoForm(){
-    const [todos, setTodos] = useState([])
+export function NewTodoForm( {onSubmit }){
+  const [newItem, setNewItem] = useState('')
 
-    
-    function handleSubmit(e){
-        e.preventDefault()
-    
-        setTodos(currentTodos=>{
-          return[
-          ...currentTodos,
-          { id:crypto.randomUUID(), title: newItem, completed: false}
-          ]
-        })
-    
-        setNewItem('')
-      }
-    
+  function handleSubmit(e){
+    e.preventDefault()
+    if (newItem === '')return
 
-    return (
-        <form onSubmit={handleSubmit} className="new-item-form">
+    onSubmit(newItem)
+
+    setNewItem('')
+  }
+    
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input 
-         value={newItem} 
-         onChange={e=>setNewItem(e.target.value)} 
-         type="text" name="item" id="item" />
+          value={newItem} 
+          onChange={e=>setNewItem(e.target.value)} 
+          type="text" name="item" id="item" 
+        />
       </div>
       <button className="btn">Add</button>
     </form>
-    )
+  )
 }
